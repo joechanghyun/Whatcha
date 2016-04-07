37a7993577948cfd038d8fce8fc3bab36a56506b
indrajch <indrach@widerplanet.com>
2016-04-07 11:21:13 +0900

    Init
    Whatcha 뭐해?
    
    Chat app project 의 탄생

---
+++ WhatchaIphone/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginContrller.swift
+//  WhatchaIphone
+//
+//  Created by 조창현 010 2075 0197 on 2016. 4. 7..
+//  Copyright © 2016년 indrajch. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ WhatchaIphone/StartController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatchaIphone
+//
+//  Created by 조창현 010 2075 0197 on 2016. 4. 7..
+//  Copyright © 2016년 indrajch. All rights reserved.
+//
+
+import UIKit
+
+class StartController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
