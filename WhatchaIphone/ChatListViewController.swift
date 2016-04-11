52b4306575b8f65a58a3c9047286d8c4f0f4abce
indrajch <indrach@widerplanet.com>
2016-04-12 00:28:40 +0900

    Update     Tabbar -> ViewController Modally 로 변경
    Add
        ChatViewController, ChatListViewController 추가

---
+++ WhatchaIphone/ChatListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatListViewController.swift
+//  WhatchaIphone
+//
+//  Created by 조창현 010 2075 0197 on 2016. 4. 12..
+//  Copyright © 2016년 indrajch. All rights reserved.
+//
+
+import UIKit
+
+class ChatListViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ WhatchaIphone/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatViewController.swift
+//  WhatchaIphone
+//
+//  Created by 조창현 010 2075 0197 on 2016. 4. 12..
+//  Copyright © 2016년 indrajch. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
