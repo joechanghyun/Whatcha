29ff5150ba8ba46582fbadb98a2ef50c2e3c4d60
indrajch <indrach@widerplanet.com>
2016-04-13 01:08:11 +0900

    Update NavigationController 로 변경, TableViewCell - CustomCell 이용하여 ChatRoomList 개발

--- WhatchaIphone/ChatListViewController.swift
+++ WhatchaIphone/ChatListViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ChatListViewController : UIViewController{
+class ChatListViewController : UITableViewController{
+    var chatRoomList: [[String:String]] = [[:]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChatRoomListCustomCell", forIndexPath: <#T##NSIndexPath#>) as! ChatRoomListCell
+        
+//        cell.roomTitleImage.image = UIImage(named:"")!
+//        cell.roomTitle.text = ""
+        
+    }
+    
+    
 }
